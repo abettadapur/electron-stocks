2c1542d3a7aaeb3322f4fae5918bb00629cb5b21
Alex Bettadapur <alexbettadapur@gmail.com>
2021-01-16 11:43:53 -0800

    Line

--- packages/frontend/src/stocks/ui/components/HistoricalGraph/HistoricalGraph.tsx
+++ packages/frontend/src/stocks/ui/components/HistoricalGraph/HistoricalGraph.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, MutableRefObject } from "react";
+import React, { useEffect, MutableRefObject, useState } from "react";
 import View from "../../view/View";
 import styled from "../../../../styled";
 import { StocksAwareState } from "frontend/stocks/redux/stocks/Stocks.types";
@@ -6,10 +6,14 @@ import { getSelectedHistoricalData } from "frontend/stocks/redux/stocks/StocksSe
 import { connect } from "react-redux";
 import IEXHistorical from "frontend/stocks/api/tiingo/models/IEXHistorical";
 import EODHistorical from "frontend/stocks/api/tiingo/models/EODHistorical";
+import { number } from "yargs";
+import { createWriteStream } from "fs-extra";
 
 type Props = {
   historicalData: IEXHistorical[] | EODHistorical[] | undefined;
 };
+
+type Point = { x: number; y: number };
 
 const GraphCanvas = styled.canvas({
   height: 200,
@@ -18,19 +22,23 @@ const GraphCanvas = styled.canvas({
 
 function HistoricalGraph(props: Props) {
   let { historicalData } = props;
+  const [mousePoint, setMousePoint] = useState<Point | null>(null);
 
   const canvasRef = React.useRef() as MutableRefObject<HTMLCanvasElement>;
   useEffect(() => {
     if (historicalData && historicalData.length > 0) {
-      drawGraph(historicalData, canvasRef);
+      drawGraph(historicalData, mousePoint, canvasRef);
     }
-  }, [historicalData]);
+  }, [historicalData, mousePoint]);
 
   return (
     <View>
       <GraphCanvas
-        height={200}
-        width={550}
+        height={900}
+        width={1600}
+        onMouseMove={(e) => {
+          setMousePoint({ x: e.clientX, y: e.clientY });
+        }}
         onMouseDown={(e) => {
           debugger;
         }}
@@ -51,6 +59,7 @@ export default connect(mapStateToProps, null)(HistoricalGraph);
 
 function drawGraph(
   data: IEXHistorical[] | EODHistorical[],
+  mousePoint: Point | null,
   canvasRef: React.MutableRefObject<HTMLCanvasElement>
 ) {
   const { min, max } = getMinMaxOverPeriod(data);
@@ -68,6 +77,22 @@ function drawGraph(
     return Math.round(index * pixelsPerIndex);
   }
 
+  function xToIndex(x: number) {
+    const pixelsPerIndex = canvasRef.current.width / (data.length - 1);
+    return Math.min(data.length - 1, Math.round(x / pixelsPerIndex));
+  }
+
+  function mousePointToCanvasPoint(point: Point): Point {
+    const boundingRect = canvasRef.current.getBoundingClientRect();
+
+    const scaleX = canvasRef.current.width / boundingRect.width; // relationship bitmap vs. element for X
+    const scaleY = canvasRef.current.height / boundingRect.height; // relationship bitmap vs. element for Y
+    return {
+      x: (point.x - boundingRect.x) * scaleX,
+      y: (point.y - boundingRect.y) * scaleY,
+    };
+  }
+
   let ctx = canvasRef.current.getContext("2d");
   if (!ctx) {
     return;
@@ -75,6 +100,7 @@ function drawGraph(
 
   ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
   ctx.beginPath();
+  ctx?.setLineDash([]);
   data.forEach((point, index) => {
     ctx.lineWidth = "2";
     ctx.strokeStyle = "green";
@@ -110,6 +136,22 @@ function drawGraph(
   ctx.fillStyle = "lightgreen";
   ctx?.fill();
   ctx?.closePath();
+
+  // Draw the point
+  if (mousePoint) {
+    const canvasPoint = mousePointToCanvasPoint(mousePoint);
+    const index = xToIndex(canvasPoint.x);
+    const dataPoint = data[index];
+
+    ctx.beginPath();
+    ctx.setLineDash([5]);
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 2;
+    ctx.moveTo(canvasPoint.x, 0);
+    ctx.lineTo(canvasPoint.x, canvasRef.current.height);
+    ctx.stroke();
+    ctx.closePath();
+  }
 }
 
 function getMinMaxOverPeriod(
